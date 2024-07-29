295ecd9db2e9adb9539ebfa9c101c568363e522d
exaon18 <exaontech@gamil.com>
2024-07-29 15:50:28 -0700

    sign in works

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 urlpatterns=[
     path('',views.index,name='index'),
-    path('dashboard/',views.dashboard,name='dashboard'),
-    path('signup/',views.signup,name='signup'),
-    path('login/',views.login,name='login'),
+   
    
     
   
--- core/views.py
+++ core/views.py
@@ -7,28 +7,3 @@ from django.contrib.auth import authenticate,login
 # Create your views here.
 def index(request):
     return render(request,'core/index.html')
-def signup(request):
-    if request.method=='POST':
-        username=request.POST['username']
-        firstname=request.POST['firstname']
-        lastname=request.POST['lastname']
-        email=request.POST['email']
-        pass1=request.POST['pass1']
-        pass2=request.POST['pass2']
-        player=User.objects.create_user(username,email,pass1)
-        
-        
-        player.first_name=firstname
-        player.last_name=lastname
-        player.save()
-        return redirect(request,'signin')
-    
-    return render(request,'core/signup.html')
-def login(request):
-    return render(request,'registration/login.html')
-def dashboard(request):
-    name=request.POST['first_name']
-    
-    return render(request,"core/dashboard.html",{
-        "name":name
-    })
