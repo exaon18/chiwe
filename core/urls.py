11f712a6bc16951d4ae915f70df8682a69213fb6
exaon18 <exaontech@gamil.com>
2024-08-11 04:26:46 -0700

    added profile function

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns=[
     path('',views.index,name='index'),
-    
+    path('home',views.index,name='home'),
+    path('singup',views.signup,name='singup'),
+    path('signout',views.singout,name='signout'),
+    path('dashboard',views.dashboard,name='dashboard'),
    
    
     
--- core/views.py
+++ core/views.py
@@ -2,8 +2,39 @@ from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login
-
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 # Create your views here.
 def index(request):
     return render(request,'core/index.html')
+def signup(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Account created for {username}!')
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = UserRegisterForm()
+    return render(request,'registration/signup.html',{'form':form})
+@login_required
+def singout(request):
+    logout(request)
+    msg='you are logged out'
+    signout=True
+    return render(request,'registration/login.html',{
+    'msg':msg,"sigout":signout
+    })
+@login_required
+def dashboard(request):
+    if request.user.is_authenticated:
+        name=request.user.username
+
+        msg='you are logged in'
+        return render(request,'core/dashboard.html',{
+        'msg':msg,"name":name
+        })
+    return render(request,'core/dashboard.html')
