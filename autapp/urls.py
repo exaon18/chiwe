9508ea250f2e1c08cb4db882f6c4e4bedae112a0
exaon18 <exaontech@gamil.com>
2025-06-01 03:28:35 -0700

    404 page fix when debug off

--- autapp/urls.py
+++ autapp/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
 from django.shortcuts import render
 
 def custom_404(request, exception):
-    return render(request, '404.html', status=404)
+    print("trying")
+    return render(request, 'custom.html', status=404)
 
 
