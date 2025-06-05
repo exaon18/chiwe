b9aa47061da19874debf2bb9294d911c41b987c3
exaon18 <exaontech@gamil.com>
2025-06-04 23:34:37 -0700

    added bonus

--- autapp/views.py
+++ autapp/views.py
@@ -153,7 +153,9 @@ def verify(request, username):
         print(user.token)
         if user.token == token:
             user.is_active = True
-            user.save()
+            userb=Ballance.objects.get(user=user)
+            userb.ballance=25
+            userb.save()
             GameHistory.objects.get_or_create(user=user,TotalPlayed=0,TotalWin=0,Totaloss=0,TotalEarning=0.00,)
             user.save()
             login(request, user)
