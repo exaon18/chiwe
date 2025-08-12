d9b0cf06d82d890ce637784f39aa763a2af3060d
exaon18 <exaontech@gamil.com>
2025-08-12 08:31:14 -0700

    bug fix

--- autapp/views.py
+++ autapp/views.py
@@ -110,7 +110,7 @@ def signup(request,ref):
         inactive_email = MyUser.objects.filter(email=email, is_active=False).first()
         if inactive_email:
             inactive_email.delete()
-        if referal:
+        if referal and referal != "1":
             try:
            
 
