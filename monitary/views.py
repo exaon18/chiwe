a31202afa12c9262f5cec8f2190cea4b6541f439
exaon18 <exaontech@gamil.com>
2025-08-19 00:57:40 -0700

    for video

--- monitary/views.py
+++ monitary/views.py
@@ -558,10 +558,10 @@ def crypto(request):
     }
 
     headers = {
-        "x-api-key": "5DR8BKJ-DYHMW8W-NRWVVRH-KP57S53"
+        "x-api-key": "9G09B6H-3F7MWMH-K2BBFAF-PT72QCF"
     }
 
-    response = requests.post("https://api.nowpayments.io/v1/invoice", json=payload, headers=headers)
+    response = requests.post("https://api-sandbox.nowpayments.io/v1/invoice", json=payload, headers=headers)
 
     if response.status_code == 200:
         print(response.text)
@@ -581,7 +581,7 @@ def webhook(request):
         return JsonResponse({"error": "Invalid method"}, status=405)
 
     try:
-        ipn_secret = "mPltdHLewEFyb2YuRaAhwvRoIUqNGIVo"
+        ipn_secret = "kJZodokihIhjFWUPgqsqdNNNyzkjLhTS"
         received_sig = request.headers.get("x-nowpayments-sig")
 
         if not received_sig:
