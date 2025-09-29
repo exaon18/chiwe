75fad628d1412e6a2c35dd2a5879aa53a1e92f55
exaon18 <exaontech@gamil.com>
2025-09-29 03:09:35 -0700

    fadfafa

--- monitary/tests.py
+++ monitary/tests.py
@@ -1,7 +1,7 @@
 import requests
 
 DEV_KEY = "7vqrbckrr4fvplcmt5ox3uqyhfxlaiwwqde3jjvt3gn1oo9ni4yyn0utxlb6e9yk"
-PAYMENT_ID = "MLoYEiuHXlv9kJbRGFmg8j3lyI3Z"
+PAYMENT_ID = "gbB5Tq4c0tvjYoVxr3jGeYigYSzt"
 HEADERS = {"Authorization": f"key {DEV_KEY}", "Content-Type": "application/json"}
 
 # GET
@@ -10,7 +10,7 @@ print("GET", r.status_code)
 print(r.text)
 
 # If GET shows transaction.txid and verified==true, POST complete:
-txid = "6e5ebe3736a14079d1ca5e9751f0519893f520da8c63cdd89c2b45527e17911f"  # from GET
+txid = "266022fd5064aaf649360451cc9f6fd9d6511eabe68fd9159cd633c5be22342f"  # from GET
 r2 = requests.post(f"https://api.minepi.com/v2/payments/{PAYMENT_ID}/complete", headers=HEADERS, json={"txid": txid}, timeout=15)
 print("POST complete", r2.status_code)
 print(r2.text)
