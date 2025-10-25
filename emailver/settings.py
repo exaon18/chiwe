4df27c98941b26387755f58da84df3bd7ee18945
exaon18 <exaontech@gamil.com>
2025-10-25 04:02:51 -0700

    db url changed

--- emailver/settings.py
+++ emailver/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = [
     '8193a66d4544.ngrok-free.app',
 ]
 
-DATABASE_URL="postgresql://chiwedb2_user:38QtqtT5KsPAoFZSa4UVLAdrQxRCGyda@dpg-d3l40gali9vc73ejor0g-a/chiwedb2"
+DATABASE_URL="postgresql://chiwedb2_user:38QtqtT5KsPAoFZSa4UVLAdrQxRCGyda@dpg-d3l40gali9vc73ejor0g-a.oregon-postgres.render.com/chiwedb2"
 # Application definition
 CORS_ALLOW_CREDENTIALS = True
 
